79eaf3a627ff02dab66ad76e7889010d25bd2a8e
Ruslan Sosa <90573501+MacEwanStudent@users.noreply.github.com>
2024-10-21 15:39:59 -0600

    Time: 9 ms (98.59%) | Memory: 33.9 MB (22.02%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -5,13 +5,9 @@ class Solution:
 
         for num in nums:
             if num in dict_counter:
-                dict_counter[num]+=1
-            else:
-                dict_counter[num]=1
-
-        for item in dict_counter.values():
-            if item>1:
                 duplicate=True
                 break
+            else:
+                dict_counter[num]=1
                 
         return duplicate
