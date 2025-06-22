ff7227e60d91efc69bd2e89cd7031f654e04cc59
Ruslan Sosa <90573501+MacEwanStudent@users.noreply.github.com>
2025-06-22 06:01:45 -0600

    Time: 11 ms (83.44%) | Memory: 20.6 MB (76.91%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,11 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        dict_anagrams = {}
-
+        groupAnagramLists= defaultdict(list)
         for word in strs:
-            sorted_word= ''.join(sorted(word))
-            if  sorted_word in dict_anagrams:
-                dict_anagrams[sorted_word].append(word)
-            else:
-                dict_anagrams[sorted_word]=[word]
-
-        return [value for value in dict_anagrams.values()]
+            sorted_word = ''.join(sorted(word))
+            groupAnagramLists[sorted_word].append(word)
+        #Converts the ouput into a List[List[str] as per instructions.
+        return list(groupAnagramLists.values())
