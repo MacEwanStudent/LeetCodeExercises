31f4dc68f13b5cc056dc20369bd374154d2a3d57
Ruslan Sosa <90573501+MacEwanStudent@users.noreply.github.com>
2025-06-22 15:40:50 -0600

    Time: 3 ms (90.96%) | Memory: 21 MB (96.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,25 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_counter = {}
-        sorted_list = []
+        frequency_counter = {}
 
         for num in nums:
-            if num in num_counter:
-                num_counter[num] += 1
+            if num in frequency_counter:
+                frequency_counter[num]+=1
             else:
-                num_counter[num] = 1
+                frequency_counter[num]=1
 
-
-        sorted_dict = dict(sorted(num_counter.items(), key=lambda item: item[1], reverse=True))
-
-        solution= []
-
-        counter = 1
-
-        for item in sorted_dict:
-            solution.append(item)
-            if counter == k:
-                break
-            counter +=1
-
-        return solution
+        return sorted(frequency_counter, key=frequency_counter.get, reverse=True)[0:k]
